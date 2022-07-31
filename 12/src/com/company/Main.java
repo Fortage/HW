793371040fb87c8bc28aef39a8a7b7ad8bcf22fb
Fortage <32445750+Fortage@users.noreply.github.com>
2022-07-31 21:56:44 +0500

    12
    init

---
+++ 12/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        test test = new test();
+    }
+
+}
