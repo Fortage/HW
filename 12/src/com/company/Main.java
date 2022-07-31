38e31d6c8f39b83933745a59626cece91520df72
Fortage <32445750+Fortage@users.noreply.github.com>
2022-07-31 21:58:02 +0500

    fix
    fix

--- 12/src/com/company/Main.java
+++ 12/src/com/company/Main.java
@@ -1,14 +1,27 @@
 package com.company;
 
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Predicate;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        test test = new test();
+        System.out.println("Введите вашу фразу");
+        Scanner in = new Scanner(System.in);
+        String sequence = in.nextLine();
+        in.close();
+        String[] words = new String[sequence.length()];
+        words = sequence.split(" ");
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words)
+        {
+            if(!map.containsKey(word))
+            {
+                map.put(word, 1);
+            }
+            else {
+                map.put(word, map.get(word) + 1);
+            }
+        }
+        System.out.println(map);
     }
-
 }
